2aa58fb411fb57d0422c92b74a6e956696b49929
Pedro Schetino <pedro.schetino.mattos@gmail.com>
2026-07-13 07:23:12 -0300

    fix(db): corrige sintaxe dos scripts de banco de dados

--- mastersys/src/main/resources/db/migration/V2__insert_dados_iniciais.sql
+++ mastersys/src/main/resources/db/migration/V2__insert_dados_iniciais.sql
@@ -25,17 +25,17 @@ SELECT id, 'Mensal', 180.00
 FROM modalidades
 WHERE nome = 'Jiu-Jitsu';
 
-INSERT INTO graduacoes (modalidade_id, nome)
+INSERT INTO graduacao (modalidade_id, nome)
 SELECT id, 'Faixa Branca'
 FROM modalidades
 WHERE nome = 'Jiu-Jitsu';
 
-INSERT INTO graduacoes (modalidade_id, nome)
+INSERT INTO graduacao (modalidade_id, nome)
 SELECT id, 'Faixa Azul'
 FROM modalidades
 WHERE nome = 'Jiu-Jitsu';
 
-INSERT INTO graduacoes (modalidade_id, nome)
+INSERT INTO graduacao (modalidade_id, nome)
 SELECT id, 'Faixa Roxa'
 FROM modalidades
 WHERE nome = 'Jiu-Jitsu';
