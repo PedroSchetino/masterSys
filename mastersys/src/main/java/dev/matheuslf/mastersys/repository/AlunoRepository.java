a0d08e6ec0a01449be0c9a954b8eb77fe5ab854b
Pedro Schetino <pedro.schetino.mattos@gmail.com>
2026-07-21 11:25:14 -0300

    feat(repository): adiciona interfaces do pacote repository

---
+++ mastersys/src/main/java/dev/matheuslf/mastersys/repository/AlunoRepository.java
@@ -0,0 +1,7 @@
+package dev.matheuslf.mastersys.repository;
+
+import dev.matheuslf.mastersys.domain.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
